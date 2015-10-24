f6414ae454f31da88891df7c48c8513ec7a959a4
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2015-10-24 12:07:58 +0200

    fix tests within umbrella app

--- apps/dt_bus/mix.exs
+++ apps/dt_bus/mix.exs
@@ -4,6 +4,8 @@ defmodule DtBus.Mixfile do
   def project do
     [app: :dt_bus,
      version: "0.0.1",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
