a92a34fa668e1288aa02ca8e868c87947461ff92
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2015-12-29 13:02:03 +0100

    fix user schema test

--- apps/dt_web/test/models/user_test.exs
+++ apps/dt_web/test/models/user_test.exs
@@ -3,16 +3,18 @@ defmodule DtWeb.UserTest do
 
   alias DtWeb.User
 
-  @valid_attrs %{email: "some content", encrypted_password: "some content", name: "some content", password: "some content"}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = User.changeset(%User{}, @valid_attrs)
+  test "login changeset with valid attributes" do
+    changeset = User.login_changeset(%User{encrypted_password: "$2b$12$YVI.NrsX3O2z5SwD4hOPt.J9yU8xlt3ns2dcMqSm3oYGVygMZQmb6"}, 
+      %{email: "some content", 
+        name: "some content", 
+        password: "password"})
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = User.changeset(%User{}, @invalid_attrs)
+  test "login changeset with invalid attributes" do
+    changeset = User.login_changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
   end
 end
