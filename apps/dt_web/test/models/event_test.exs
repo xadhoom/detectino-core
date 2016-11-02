40ff92d699195398a9a0ac19fd27fd0c9efa6b54
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2016-11-02 22:22:28 +0100

    validate event specific config

--- apps/dt_web/test/models/event_test.exs
+++ apps/dt_web/test/models/event_test.exs
@@ -3,16 +3,80 @@ defmodule DtWeb.EventTest do
 
   alias DtWeb.Event
 
-  @valid_attrs %{name: "some content", source: "EMAIL"}
+  @valid_attrs %{name: "some content", source: ""}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = Event.create_changeset(%Event{}, @valid_attrs)
+  test "changeset with invalid source" do
+    attrs = %{name: "a name", source: "whatever"}
+    changeset = Event.create_changeset(%Event{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset with valid source, no config" do
+    attrs = %{name: "a name", source: "sensor"}
+    changeset = Event.create_changeset(%Event{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "malformed source config" do
+    attrs = %{name: "name", source: "partition", source_config: "aye"}
+    changeset = Event.create_changeset(%Event{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset for a partition event" do
+    sconf = %{name: "a name", type: "alarm"}
+    |> Poison.encode!
+    attrs = %{name: "name", source: "partition", source_config: sconf}
+    changeset = Event.create_changeset(%Event{}, attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = Event.create_changeset(%Event{}, @invalid_attrs)
+  test "changeset for a partition event: missing type" do
+    sconf = %{name: "a name"}
+    |> Poison.encode!
+    attrs = %{name: "name", source: "partition", source_config: sconf}
+    changeset = Event.create_changeset(%Event{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset for a partition event: missing name" do
+    sconf = %{type: "alarm"}
+    |> Poison.encode!
+    attrs = %{name: "name", source: "partition", source_config: sconf}
+    changeset = Event.create_changeset(%Event{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset for a sensor event" do
+    sconf = %{address: "1", port: 1, type: "alarm"}
+    |> Poison.encode!
+    attrs = %{name: "name", source: "sensor", source_config: sconf}
+    changeset = Event.create_changeset(%Event{}, attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset for a sensor event: missing address" do
+    sconf = %{port: 1, type: "alarm"}
+    |> Poison.encode!
+    attrs = %{name: "name", source: "sensor", source_config: sconf}
+    changeset = Event.create_changeset(%Event{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset for a sensor event: missing port" do
+    sconf = %{address: "1", type: "alarm"}
+    |> Poison.encode!
+    attrs = %{name: "name", source: "sensor", source_config: sconf}
+    changeset = Event.create_changeset(%Event{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset for a sensor event: missing type" do
+    sconf = %{address: "1", port: 1}
+    |> Poison.encode!
+    attrs = %{name: "name", source: "sensor", source_config: sconf}
+    changeset = Event.create_changeset(%Event{}, attrs)
     refute changeset.valid?
   end
 end
--- apps/dt_web/web/models/event.ex
+++ apps/dt_web/web/models/event.ex
@@ -13,6 +13,9 @@ end
 defmodule DtWeb.Event do
   use DtWeb.Web, :model
 
+  alias DtWeb.Event.SensorEvConf
+  alias DtWeb.Event.PartitionEvConf
+
   @derive {Poison.Encoder, only: [:id, :name, :description, :source]}
   schema "events" do
     field :name, :string
@@ -25,17 +28,78 @@ defmodule DtWeb.Event do
     many_to_many :outputs, DtWeb.Output, join_through: DtWeb.EventOutput
   end
 
-  @required_fields ~w(name source)
-  @optional_fields ~w(description source_config)
+  @optional_fields ~w(description)
+  @required_fields ~w(name source source_config)
+  @validate_required Enum.map(@required_fields, fn(x) -> String.to_atom(x) end)
+  @source_types ["sensor", "partition"]
 
   def create_changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_required(@validate_required)
+    |> validate_inclusion(:source, @source_types)
+    |> check_config
   end
 
   def update_changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_required(@validate_required)
+    |> validate_inclusion(:source, @source_types)
+    |> check_config
   end
 
+  defp check_config(changeset) do
+    case fetch_change(changeset, :source_config) do
+      {:ok, change} ->
+        cast_config(changeset, change)
+      :error ->
+        add_error(changeset, :source_config, "invalid")
+    end
+  end
+
+  defp cast_config(changeset, change) do
+    case fetch_change(changeset, :source) do
+      {:ok, "sensor"} ->
+        ret = Poison.decode(change, as: %SensorEvConf{})
+        validate_config(changeset, ret)
+      {:ok, "partition"} ->
+        ret = Poison.decode(change, as: %PartitionEvConf{})
+        validate_config(changeset, ret)
+      _ ->
+        add_error(changeset, :source, "invalid")
+    end
+  end
+
+  defp validate_config(changeset, {:error, _}) do
+    add_error(changeset, :source_config, "format, cannot parse")
+  end
+
+  defp validate_config(changeset, {:ok, %SensorEvConf{address: nil}}) do
+    add_error(changeset, :source_config, "invalid sensor config format: address")
+  end
+
+  defp validate_config(changeset, {:ok,  %SensorEvConf{port: nil}}) do
+    add_error(changeset, :source_config, "invalid sensor config format: port")
+  end
+
+  defp validate_config(changeset, {:ok, %SensorEvConf{type: nil}}) do
+    add_error(changeset, :source_config, "invalid sensor config format: type")
+  end
+
+  defp validate_config(changeset, {:ok, %SensorEvConf{}}) do
+    changeset
+  end
+
+  defp validate_config(changeset, {:ok, %PartitionEvConf{name: nil}}) do
+    add_error(changeset, :source_config, "invalid partition config format: name")
+  end
+
+  defp validate_config(changeset, {:ok, %PartitionEvConf{type: nil}}) do
+    add_error(changeset, :source_config, "invalid partition config format: type")
+  end
+
+  defp validate_config(changeset, {:ok, %PartitionEvConf{}}) do
+    changeset
+  end
 end
