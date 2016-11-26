0b65b50a761b3d3d42e0bd9ca1ec661e853e7f6f
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2016-11-26 11:32:32 +0100

    chore: clean up comments

--- apps/dt_core/mix.exs
+++ apps/dt_core/mix.exs
@@ -20,8 +20,6 @@ defmodule DtCore.Mixfile do
   end
 
   # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     case Mix.env do
       :test -> [applications: [:logger, :swoosh, :etimer]]
@@ -34,19 +32,6 @@ defmodule DtCore.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:ecto, "~> 2.0"},
