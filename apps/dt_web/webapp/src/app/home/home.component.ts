15d5c0df28275690febf923a4db3d2ec837fdd15
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2016-12-04 23:53:38 +0100

    load scenarios that can be armed on the home

--- apps/dt_web/webapp/src/app/home/home.component.ts
+++ apps/dt_web/webapp/src/app/home/home.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ScenarioService, NotificationService } from '../services';
+
+import { Scenario } from '../models/scenario';
 
 @Component({
   selector: 'home',
-  styles: [ require('./home.component.css') ],
+  styles: [require('./home.component.css')],
   template: require('./home.component.html')
 })
-export class Home {
+
+export class Home implements OnInit {
+
+  scenarios: Scenario[];
+
+  errorMessage: string;
+
+  constructor(private scenarioService: ScenarioService,
+    private notificationService: NotificationService) { };
+
   ngOnInit() {
     console.log('hello `Home` component');
+    this.scenarios = [];
+    this.scenarioService.get_available().subscribe(
+      scenarios => this.setScenarios(scenarios),
+      error => this.onError(error)
+    );
+  }
+
+  onError(error: any) {
+    this.errorMessage = <any>error;
+    this.notificationService.error(this.errorMessage);
+  }
+
+  private setScenarios(scenarios) {
+    this.scenarios = scenarios;
   }
 }
