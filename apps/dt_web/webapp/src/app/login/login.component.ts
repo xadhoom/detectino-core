d26fc01e9e04f0df42a9637f47a0398f079164c6
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2016-12-04 23:19:09 +0100

    navigate to home after login

--- apps/dt_web/webapp/src/app/login/login.component.ts
+++ apps/dt_web/webapp/src/app/login/login.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../services';
 
 @Component({
   selector: 'login',
   template: require('./login.component.html'),
-  styles: [ require('./login.component.css') ]
+  styles: [require('./login.component.css')]
 })
 
 export class Login {
-  constructor(public auth: AuthService) {
+  constructor(public auth: AuthService, private router: Router) {
   }
 
   login(event: Event, username: string, password: string) {
     event.preventDefault();
     this.auth.login(username, password).
-      subscribe( res => {
-      if (res) {
-        // this.router.parent.navigate(['Home']);
-      } else {
-        console.log('ayee cannot log');
-      }
-    }, error => console.log('ayeee error in log'));
+      subscribe(res => {
+        if (res) {
+          this.router.navigateByUrl('/home');
+        } else {
+          console.log('ayee cannot log');
+        }
+      }, error => console.log('ayeee error in log'));
   }
 }
