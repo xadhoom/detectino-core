ea51acf7046c05196a6f671a3ad1f1a57d201e5a
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2016-12-22 22:29:17 +0100

    protect scenarios with a pin, too

--- apps/dt_web/webapp/src/app/scenarios/scenariolist.component.ts
+++ apps/dt_web/webapp/src/app/scenarios/scenariolist.component.ts
@@ -1,40 +1,40 @@
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 
-import { ScenarioService, NotificationService } from '../services';
+import { ScenarioService, NotificationService, PinService } from '../services';
 import { Scenario } from '../models/scenario';
 
 @Component({
-    selector: 'dt-scenariolist',
-    encapsulation: ViewEncapsulation.None,
-    styleUrls: ['./scenariolist.component.css'],
-    templateUrl: './scenariolist.component.html'
+  selector: 'dt-scenariolist',
+  styleUrls: ['./scenariolist.component.css'],
+  templateUrl: './scenariolist.component.html'
 })
 
 export class Scenariolist implements OnInit {
-    scenarios: Scenario[];
-    errorMessage: string;
+  scenarios: Scenario[];
+  errorMessage: string;
 
-    constructor(private scenarioService: ScenarioService,
-        private notificationService: NotificationService) { }
+  constructor(private scenarioService: ScenarioService,
+    private notificationService: NotificationService,
+    private pinSrv: PinService) { }
 
-    ngOnInit() {
-        this.scenarios = [];
-        this.scenarioService.get_available().subscribe(
-            scenarios => this.setScenarios(scenarios),
-            error => this.onError(error)
-        );
-    }
+  ngOnInit() {
+    this.scenarios = [];
+    this.scenarioService.get_available().subscribe(
+      scenarios => this.setScenarios(scenarios),
+      error => this.onError(error)
+    );
+  }
 
-    getInitials(name: string) {
-        return name[0];
-    }
+  getInitials(name: string) {
+    return name[0];
+  }
 
-    onError(error: any) {
-        this.errorMessage = <any>error;
-        this.notificationService.error(this.errorMessage);
-    }
+  onError(error: any) {
+    this.errorMessage = <any>error;
+    this.notificationService.error(this.errorMessage);
+  }
 
-    private setScenarios(scenarios) {
-        this.scenarios = scenarios;
-    }
+  private setScenarios(scenarios) {
+    this.scenarios = scenarios;
+  }
 }
