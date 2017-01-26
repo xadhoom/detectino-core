1007e3bba76b01046910844fbc24bf099827767b
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-01-26 13:57:07 +0100

    prepare for event dispatcher

---
+++ lib/dt_core/event_dispatcher.ex
@@ -0,0 +1,9 @@
+defmodule DtCore.EventDispatcher do
+  @moduledoc """
+  Receives event from Sensors and Partition Workers and
+  dispatches to Outputs using DtCore.EvRegistry.
+
+  It also logs event sequences and provides a the event
+  stream to subscribed listeners.
+  """
+end
