226b51a464d485cd8df4452cd3e59dba324ac1f5
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-01-26 23:34:23 +0100

    prepare for dispatching to other listeners

--- lib/dt_core/event_bridge.ex
+++ lib/dt_core/event_bridge.ex
@@ -28,6 +28,10 @@ defmodule DtCore.EventBridge do
     GenServer.cast(__MODULE__, {:dispatch, key, payload})
   end
 
+  def start_listening(filter_fun \\ fn(_) -> true end) do
+    GenServer.call(__MODULE__, {:start_listening, self(), filter_fun})
+  end
+
   #
   # GenServer callbacks
   #
@@ -40,6 +44,34 @@ defmodule DtCore.EventBridge do
     Registry.dispatch(OutputsRegistry.registry, key, fn listeners ->
       for {pid, _} <- listeners, do: send(pid, payload)
     end)
+    dispatch_filtered(state, {key, payload})
     {:noreply, state}
+  end
+
+  def handle_call({:start_listening, pid, filter_fun}, _from, state) do
+    #key = Base.encode64 :erlang.term_to_binary(pid)
+    listeners = Map.put state.listeners, pid, %{filter: filter_fun}
+    Process.monitor pid
+    {:reply, {:ok, pid}, %DtCore.EventBridge{state | listeners: listeners}}
+  end
+
+  def handle_call({:stop_listening, pid}, _from, state) do
+    #key = Base.encode64 :erlang.term_to_binary(pid)
+    listeners = Map.delete state.listeners, pid
+    Process.unlink pid
+    {:reply, {:ok, pid}, %DtCore.EventBridge{state | listeners: listeners}}
+  end
+
+  def handle_info({:DOWN, _, _, pid, _}, state) do
+    handle_call {:stop_listening, pid}, nil, state
+    {:noreply, state}
+  end
+
+  defp dispatch_filtered(state, {key, payload}) do
+    Enum.each state.listeners, fn({pid, v}) ->
+      if v.filter.({key, payload}) do
+        send pid, {:bridge, key, payload}
+      end
+    end
   end
 end
