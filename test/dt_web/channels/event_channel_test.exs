0d8fcc97acf1449416f64a013fc3adcd24f69f89
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-02-02 23:50:59 +0100

    initial event channel to push realtime alarm events to clients

--- test/dt_web/channels/event_channel_test.exs
+++ test/dt_web/channels/event_channel_test.exs
@@ -1,11 +1,77 @@
 defmodule DtWeb.EventChannelTest do
-  use DtWeb.ChannelCase
+  use DtWeb.ChannelCase, async: false
 
-  alias DtWeb.Channels.Event
+  import Supervisor.Spec
+
+  alias DtCore.Event, as: Event
+  alias DtWeb.Channels.Event, as: ChannelEvent
+  alias DtCore.Sensor.Partition
+  alias DtCore.Sensor.PartitionSup
+  alias DtWeb.Sensor, as: SensorModel
+  alias DtWeb.Partition, as: PartitionModel
   # alias DtCore.OutputsRegistry
 
   test "can join arm topic" do
-    assert {:ok, :state} = Event.join("event:arm", nil, :state)
+    assert {:ok, :state} = ChannelEvent.join("event:arm", nil, :state)
+  end
+
+  test "gets idle status" do
+    start_idle_partition()
+
+    {:ok, _, _socket} = socket()
+    |> subscribe_and_join(ChannelEvent, "event:alarm", %{})
+
+    assert_push "event", %{alarmed: false}
+  end
+
+  test "gets alarm status" do
+    start_alarmed_partition()
+
+    {:ok, _, _socket} = socket()
+    |> subscribe_and_join(ChannelEvent, "event:alarm", %{})
+
+    assert_push "event", %{alarmed: true}
+  end
+
+  defp start_idle_partition do
+    cache = :ets.new(:part_state_cache, [:set, :public])
+    ctx = [cache: cache]
+    start_partition(ctx)
+  end
+
+  defp start_alarmed_partition do
+    cache = :ets.new(:part_state_cache, [:set, :public])
+    ctx = [cache: cache]
+    {:ok, pid, part} = start_partition(ctx)
+    alarm_partition(pid, part)
+  end
+
+  defp alarm_partition(pid, part) do
+    :ok = Partition.arm(part, "ARM")
+
+    ev = %Event{address: "1", port: 1, value: 15}
+    :ok = Process.send(pid, {:event, ev}, [])
+    ev = %Event{address: "2", port: 1, value: 15}
+    :ok = Process.send(pid, {:event, ev}, [])
+  end
+
+  defp start_partition(ctx) do
+    sensors = [
+      %SensorModel{name: "A", balance: "NC", th1: 10,
+        partitions: [], enabled: true, address: "1", port: 1},
+      %SensorModel{name: "B", balance: "NC", th1: 10,
+        partitions: [], enabled: true, address: "2", port: 1}
+      ]
+    part = %PartitionModel{name: "prot", armed: "DISARM",
+      sensors: sensors}
+
+    {:ok, sup_pid} = PartitionSup.start_link()
+
+    {ret, pid} = Supervisor.start_child(sup_pid,
+      worker(Partition,[{part, ctx[:cache]}],
+      restart: :transient, id: part.name))
+
+    {ret, pid, part}
   end
 
 """
--- web/channels/event.ex
+++ web/channels/event.ex
@@ -1,7 +1,7 @@
 defmodule DtWeb.Channels.Event do
   use Phoenix.Channel
 
-  #alias DtCore.EvRegistry
+  alias DtCore.StatusTracker
 
   require Logger
 
@@ -10,9 +10,20 @@ defmodule DtWeb.Channels.Event do
   end
 
   def join("event:alarm", _message, socket) do
-    #Registry.register(EvRegistry.registry, %{}, [])
-    #Registry.register(EvRegistry.registry, %{source: :sensor}, [])
+    send(self(), :after_join_alarm)
     {:ok, socket}
+  end
+
+  def handle_info(:after_join_alarm, socket) do
+    Etimer.start_link(socket)
+    push_status(socket)
+    {:noreply, socket}
+  end
+
+  def push_status(socket) do
+    alarmed = StatusTracker.alarm_status()
+    push socket, "event", %{alarmed: alarmed}
+    Etimer.start_timer(socket, :time, 1000, {__MODULE__, :push_status, [socket]})
   end
 
 end
