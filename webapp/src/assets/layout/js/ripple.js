ac2a3170331c930409ae419e01b73056333abc05
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-02-07 23:34:38 +0100

    update to latest cli, primeng and ultima theme

--- webapp/src/assets/layout/js/ripple.js
+++ webapp/src/assets/layout/js/ripple.js
@@ -13,8 +13,8 @@ $(function() {
         }
              
         ink = $(this).find(".ink");
-        ink.removeClass("animate");
-         
+        ink.removeClass("ripple-animate");
+                 
         if(!ink.height() && !ink.width()){
             d = Math.max($(this).outerWidth(), $(this).outerHeight());
             ink.css({height: d, width: d});
@@ -23,6 +23,6 @@ $(function() {
         x = e.pageX - $(this).offset().left - ink.width()/2;
         y = e.pageY - $(this).offset().top - ink.height()/2;
          
-        ink.css({top: y+'px', left: x+'px', 'pointer-events': 'none'}).addClass("animate");
+        ink.css({top: y+'px', left: x+'px', 'pointer-events': 'none'}).addClass("ripple-animate");
     });
 });
