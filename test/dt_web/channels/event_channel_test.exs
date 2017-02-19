96011b9fba03dfd2e6af1e602705fde764f85093
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-02-19 10:17:36 +0100

    chore: remove old commented code

--- test/dt_web/channels/event_channel_test.exs
+++ test/dt_web/channels/event_channel_test.exs
@@ -91,21 +91,4 @@ defmodule DtWeb.EventChannelTest do
     {ret, pid, part}
   end
 
-"""
-  test "alarm topic listens to any alarm event" do
-    {:ok, _} = Registry.start_link(:duplicate, OutputsRegistry.registry)
-
-    {:ok, _, _socket} = socket()
-    |> subscribe_and_join(Event, "event:alarm", %{})
-
-    key = %{source: :partition}
-    listeners = Registry.lookup(OutputsRegistry.registry, key)
-    assert Enum.count(listeners) == 1
-
-    key = %{source: :sensor}
-    listeners = Registry.lookup(OutputsRegistry.registry, key)
-    assert Enum.count(listeners) == 1
-  end
-"""
-
 end
