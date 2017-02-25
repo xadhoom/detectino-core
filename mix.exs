61022e30365775c51b68c5df006aeb4564313df8
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-02-25 22:18:52 +0100

    chore: update mix deps

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Detectino.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
+     {:phoenix_ecto, "~> 3.2"},
      {:postgrex, "~> 0.11"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
