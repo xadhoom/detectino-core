5050e3c2e3a913c7b8a9fab44f7116b8728da89b
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-03-16 19:05:32 +0100

    reuse code in tests

--- test/dt_core/sensor/worker_test.exs
+++ test/dt_core/sensor/worker_test.exs
@@ -10,11 +10,8 @@ defmodule DtCore.Test.Sensor.Worker do
   @arm_disarmed "DISARM"
   @arm_armed "ARM"
 
-  test "no alarm but simple events if partion is not armed" do
-    part = %PartitionModel{name: "prot", armed: @arm_disarmed}
-    config = %SensorModel{name: "NC", balance: "NC", th1: 10,
-      partitions: [], enabled: true, address: "1", port: 1}
-    {:ok, pid} = Worker.start_link({config, part, self()})
+  test "no alarm but simple events if NC partion is not armed" do
+    {:ok, part, config, pid} = setup_nc()
 
     ev = %Event{address: "1", port: 1, value: 15}
     :ok = Process.send(pid, {:event, ev, part}, [])
@@ -27,11 +24,8 @@ defmodule DtCore.Test.Sensor.Worker do
     assert :standby == Worker.alarm_status({config, part})
   end
 
-  test "disarm an already disarmed sensor" do
-    part = %PartitionModel{name: "prot", armed: @arm_disarmed}
-    config = %SensorModel{name: "NC", balance: "NC", th1: 10,
-      partitions: [], enabled: true, address: "1", port: 1}
-    {:ok, pid} = Worker.start_link({config, part, self()})
+  test "disarm an already disarmed NC sensor" do
+    {:ok, part, config, pid} = setup_nc()
 
     disarm_cmd = {:disarm}
     :ok = GenServer.call(pid, disarm_cmd)
@@ -39,11 +33,8 @@ defmodule DtCore.Test.Sensor.Worker do
     assert :standby == Worker.alarm_status({config, part})
   end
 
-  test "triggers alarm on immediate sensor" do
-    part = %PartitionModel{name: "part1", armed: @arm_armed}
-    config = %SensorModel{name: "NC", balance: "NC", th1: 10,
-      partitions: [], enabled: true, address: "1", port: 1}
-    {:ok, pid} = Worker.start_link({config, part, self()})
+  test "triggers alarm on immediate NC sensor" do
+    {:ok, part, config, pid} = setup_nc()
 
     arm_cmd = {:arm, part.exit_delay}
     :ok = GenServer.call(pid, arm_cmd)
@@ -112,23 +103,7 @@ defmodule DtCore.Test.Sensor.Worker do
   end
 
   test "alarm on entry is delayed by partion time if sensor is a delayed one" do
-    part = %PartitionModel{
-      name: "part1",
-      armed: @arm_armed,
-      entry_delay: 1
-      }
-    config = %SensorModel{
-      name: "sense2",
-      address: "1",
-      port: 1,
-      balance: "NC",
-      th1: 10,
-      entry_delay: true,
-      exit_delay: true,
-      partitions: [],
-      enabled: true
-    }
-    {:ok, pid} = Worker.start_link({config, part, self()})
+     {:ok, part, config, pid} = setup_delayed_nc(1, false)
 
     arm_cmd = {:arm, part.exit_delay}
     :ok = GenServer.call(pid, arm_cmd)
@@ -148,21 +123,8 @@ defmodule DtCore.Test.Sensor.Worker do
   end
 
   test "delayed alarm on entry is cancelled if partition is unarmed in time" do
-    part = %PartitionModel{
-      name: "part1",
-      armed: @arm_armed,
-      entry_delay: 60
-      }
-    config = %SensorModel{
-      name: "sense1",
-      balance: "NC",
-      th1: 10,
-      entry_delay: true,
-      partitions: [],
-      address: "1", port: 1,
-      enabled: true
-    }
-    {:ok, pid} = Worker.start_link({config, part, self()})
+    {:ok, part, config, pid} = setup_delayed_nc(false, 60)
+
     arm_cmd = {:arm, 0}
     :ok = GenServer.call(pid, arm_cmd)
 
@@ -186,21 +148,8 @@ defmodule DtCore.Test.Sensor.Worker do
   end
 
   test "delayed alarm on exit is cancelled if partition is unarmed in time" do
-    part = %PartitionModel{
-      name: "part1",
-      armed: @arm_armed,
-      exit_delay: 60
-      }
-    config = %SensorModel{
-      name: "sense1",
-      balance: "NC",
-      th1: 10,
-      exit_delay: true,
-      partitions: [],
-      address: "1", port: 1,
-      enabled: true
-    }
-    {:ok, pid} = Worker.start_link({config, part, self()})
+    {:ok, part, config, pid} = setup_delayed_nc(false, 60)
+
     arm_cmd = {:arm, 0}
     :ok = GenServer.call(pid, arm_cmd)
 
@@ -226,23 +175,7 @@ defmodule DtCore.Test.Sensor.Worker do
   end
 
   test "delayed alarm on exit" do
-    part = %PartitionModel{
-      name: "part1",
-      armed: @arm_armed,
-      entry_delay: 30,
-      exit_delay: 1
-      }
-    config = %SensorModel{
-      name: "sense1",
-      balance: "NC",
-      th1: 10,
-      entry_delay: true,
-      exit_delay: true,
-      partitions: [],
-      address: "1", port: 1,
-      enabled: true
-    }
-    {:ok, pid} = Worker.start_link({config, part, self()})
+    {:ok, part, config, pid} = setup_delayed_nc(30, 1)
 
     arm_cmd = {:arm, part.exit_delay}
     :ok = GenServer.call(pid, arm_cmd)
@@ -261,6 +194,35 @@ defmodule DtCore.Test.Sensor.Worker do
     {:start, %SensorEv{type: :alarm, address: "1", port: 1, delayed: false}}
     |> assert_receive(5000)
     assert :alarm == Worker.alarm_status({config, part})
+  end
+
+  defp setup_nc do
+    partition = %PartitionModel{name: "NCPART", armed: @arm_disarmed}
+    sensor = %SensorModel{name: "NCSENSOR", balance: "NC", th1: 10,
+      partitions: [], enabled: true, address: "1", port: 1}
+    {:ok, pid} = Worker.start_link({sensor, partition, self()})
+    {:ok, partition, sensor, pid}
+  end
+
+  defp setup_delayed_nc(entry_delay, exit_delay) do
+    partition = %PartitionModel{
+      name: "PART_DELAYED",
+      armed: @arm_armed,
+      entry_delay: entry_delay,
+      exit_delay: exit_delay
+      }
+    sensor = %SensorModel{
+      name: "NC_DELAYED",
+      balance: "NC",
+      th1: 10,
+      entry_delay: is_integer(entry_delay),
+      exit_delay: is_integer(exit_delay),
+      partitions: [],
+      address: "1", port: 1,
+      enabled: true
+    }
+    {:ok, pid} = Worker.start_link({sensor, partition, self()})
+    {:ok, partition, sensor, pid}
   end
 
 end
