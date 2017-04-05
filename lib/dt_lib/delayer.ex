21534044ca851cb284695b6776a2677e84cad1f7
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-04-06 00:01:23 +0200

    add a delayer to help spitting back terms when is convenient

---
+++ lib/dt_lib/delayer.ex
@@ -0,0 +1,126 @@
+defmodule DtLib.Delayer.Unit do
+  defstruct ref: nil,
+    term: nil,
+    delay: nil,
+    started_at: nil
+end
+
+defmodule DtLib.Delayer do
+  @moduledoc """
+  A simple term storage which spits back
+  them after an user choosen delay.
+  Each stored term has it's own delay.
+  """
+  use GenServer
+
+  alias DtLib.Delayer
+  alias DtLib.Delayer.Unit
+
+  defstruct terms: [],
+    offset: 0,
+    recipient: nil
+
+  @granularity 10 # in msecs
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, self())
+  end
+
+  @spec put(any(), any(), 0) :: :error
+  def put(_, _, 0) do
+    :error
+  end
+
+  @spec put(pid(), any(), non_neg_integer()) :: {:ok, reference()}
+  def put(server, term, delay) do
+    GenServer.call(server, {:put, term, delay})
+  end
+
+  @doc false
+  def tick(server) do
+    GenServer.call(server, {:tick})
+  end
+
+  @spec warp(pid(), non_neg_integer()) :: :warped
+  def warp(server, offset) do
+    GenServer.call(server, {:warp, offset})
+  end
+
+  # GenServer callbacks
+
+  @doc false
+  def init(recipient) do
+    start_ticker()
+    state = %Delayer{recipient: recipient}
+    {:ok, state}
+  end
+
+  @doc false
+  def handle_call({:warp, offset}, _from, state) do
+    Etimer.stop_timer(self(), :tick)
+    state = %{state | offset: offset}
+    terms = process_entries(state)
+    reschedule_tick()
+    state = %{state | terms: terms}
+    {:reply, :warped, state}
+  end
+
+  @doc false
+  def handle_call({:put, term, delay}, _from, state) do
+    ref = make_ref()
+    entry = %Unit{ref: ref,
+      term: term,
+      delay: delay,
+      started_at: System.monotonic_time()
+    }
+    terms = [entry | state.terms]
+    {:reply, {:ok, ref}, %{state | terms: terms}}
+  end
+
+  @doc false
+  def handle_call({:tick}, _from, state) do
+    terms = process_entries(state)
+    reschedule_tick()
+    {:reply, :ok, %{state | terms: terms}}
+  end
+
+  defp process_entries(state) do
+    Enum.reject(state.terms, fn(term) ->
+      check_expired(term, state)
+    end)
+  end
+
+  defp start_ticker do
+    server_name = self()
+    Etimer.start_link(server_name)
+    reschedule_tick()
+  end
+
+  defp reschedule_tick do
+    server_name = self()
+    Etimer.start_timer(server_name, :tick, @granularity,
+      {__MODULE__, :tick, [server_name]})
+  end
+
+  defp check_expired(term, state) do
+    delay = term.delay
+    now = System.monotonic_time()
+    delta = System.convert_time_unit(now - term.started_at,
+      :native, :millisecond)
+    # we may have an offset, so apply it
+    delta = delta + state.offset
+
+    case delta do
+      v when delay <= v ->
+        release_term(term, state)
+        true
+      _ ->
+        false
+    end
+  end
+
+  defp release_term(term, state) do
+    send(state.recipient, term.term)
+  end
+
+end
---
+++ test/dt_lib/delayer_test.exs
@@ -0,0 +1,104 @@
+defmodule DtLib.Test.Delayer do
+  use ExUnit.Case, async: true
+
+  alias DtLib.Delayer
+
+  test "can start link" do
+    {:ok, pid} = Delayer.start_link()
+    assert is_pid(pid)
+  end
+
+  test "put some terms into it" do
+    refs = []
+    {:ok, pid} = Delayer.start_link()
+
+    {:ok, ref} = Delayer.put(pid, :atom, 100)
+    assert is_reference(ref)
+    refs = [ref | refs]
+
+    {:ok, ref} = Delayer.put(pid, "a string", 1)
+    assert is_reference(ref)
+    refs = [ref | refs]
+
+    {:ok, ref} = Delayer.put(pid, [:a, :list], 10)
+    assert is_reference(ref)
+    refs = [ref | refs]
+
+    {:ok, ref} = Delayer.put(pid, %{im: "a map"}, 42)
+    assert is_reference(ref)
+    refs = [ref | refs]
+
+    {:ok, ref} = Delayer.put(pid, {:tuple, :for, "you"}, 77)
+    assert is_reference(ref)
+    refs = [ref | refs]
+
+    refs = Enum.uniq(refs)
+    assert Enum.count(refs) == 5
+  end
+
+  test "cannot add with delay 0" do
+    assert :error == Delayer.put(:any, :any, 0)
+  end
+
+  test "warp the time" do
+    {:ok, pid} = Delayer.start_link()
+
+    {:ok, _ref} = Delayer.put(pid, :atom, 1000)
+    :warped = Delayer.warp(pid, 1000)
+    assert_received :atom
+  end
+
+  test "normal time (longer test...)" do
+    {:ok, pid} = Delayer.start_link()
+
+    {:ok, _ref} = Delayer.put(pid, :atom, 1000)
+    assert_receive :atom, 2000
+  end
+
+  test "different terms have different delays" do
+    {:ok, pid} = Delayer.start_link()
+
+    {:ok, _ref} = Delayer.put(pid, :a, 1000)
+    {:ok, _ref} = Delayer.put(pid, :b, 2000)
+    {:ok, _ref} = Delayer.put(pid, :c, 3000)
+    {:ok, _ref} = Delayer.put(pid, :d, 4000)
+
+    # first check
+    :warped = Delayer.warp(pid, 1000)
+    assert_received :a
+    refute_received :b
+    refute_received :c
+    refute_received :d
+
+    # second check
+    :warped = Delayer.warp(pid, 2000)
+    assert_received :b
+    refute_received :c
+    refute_received :d
+
+    # 3rd check
+    :warped = Delayer.warp(pid, 3000)
+    assert_received :c
+    refute_received :d
+
+    # final check
+    :warped = Delayer.warp(pid, 4000)
+    assert_received :d
+  end
+
+  test "one warp to rule them all" do
+    {:ok, pid} = Delayer.start_link()
+
+    {:ok, _ref} = Delayer.put(pid, :a, 1000)
+    {:ok, _ref} = Delayer.put(pid, :b, 2000)
+    {:ok, _ref} = Delayer.put(pid, :c, 3000)
+    {:ok, _ref} = Delayer.put(pid, :d, 4000)
+
+    :warped = Delayer.warp(pid, 5000)
+    assert_received :a
+    assert_received :b
+    assert_received :c
+    assert_received :d
+  end
+
+end
