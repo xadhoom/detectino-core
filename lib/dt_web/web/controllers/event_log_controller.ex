61c00c684de8a1f092a962a0df34f59e1acf39cb
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-08-08 16:08:44 +0200

    drop duplicated function

--- lib/dt_web/web/controllers/event_log_controller.ex
+++ lib/dt_web/web/controllers/event_log_controller.ex
@@ -17,15 +17,6 @@ defmodule DtWeb.EventLogController do
   plug EnsureAuthenticated, [handler: SessionController]
   plug PinAuthorize
 
-  def index(conn, params) do
-    order = [:inserted_at]
-    case Crud.all(conn, params, {Repo, EventLog, order}) do
-      {:ok, conn, items} ->
-        render(conn, items: items)
-      {:error, conn, code} ->
-        send_resp(conn, code, StatusCodes.status_code(code))
-    end
-  end
 
   def create(conn, _params) do
     send_resp(conn, 501, StatusCodes.status_code(501))
