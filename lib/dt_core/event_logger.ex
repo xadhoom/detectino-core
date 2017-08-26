02e9b42e0152a1968d593a706c98f582063f6929
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-08-26 15:48:34 +0200

    don't save realtime and idle events, but log them anyway. this to provide a cleaner view on gui, with details still being present in log files

--- lib/dt_core/event_logger.ex
+++ lib/dt_core/event_logger.ex
@@ -26,7 +26,12 @@ defmodule DtCore.EventLogger do
     {:ok, nil}
   end
 
-  def handle_info({:bridge_ev, _, {op, ev = %ArmEv{}}}, state) do
+  def handle_info(msg = {:bridge_ev, _, e}, state) do
+    Logger.info fn() -> "EVENT: #{inspect e}" end
+    on_info(msg, state)
+  end
+
+  defp on_info({:bridge_ev, _, {op, ev = %ArmEv{}}}, state) do
     operation = Atom.to_string(op)
     %{type: "arm", operation: operation, details: ev}
     |> save_eventlog()
@@ -34,7 +39,7 @@ defmodule DtCore.EventLogger do
     {:noreply, state}
   end
 
-  def handle_info({:bridge_ev, _, {op, ev = %ExitTimerEv{}}}, state) do
+  defp on_info({:bridge_ev, _, {op, ev = %ExitTimerEv{}}}, state) do
     operation = Atom.to_string(op)
     %{type: "exit_timer", operation: operation, details: ev}
     |> save_eventlog()
@@ -42,11 +47,12 @@ defmodule DtCore.EventLogger do
     {:noreply, state}
   end
 
-  def handle_info({:bridge_ev, _, {_, %DetectorEv{type: :reading}}}, state) do
+  defp on_info({:bridge_ev, _, {_, %DetectorEv{type: type}}}, state)
+      when type in [:idle, :realtime] do
     {:noreply, state}
   end
 
-  def handle_info({:bridge_ev, _, {op, ev = %DetectorEv{}}}, state) do
+  defp on_info({:bridge_ev, _, {op, ev = %DetectorEv{}}}, state) do
     operation = Atom.to_string(op)
     %{type: "alarm", operation: operation, details: ev}
     |> save_eventlog()
@@ -54,7 +60,7 @@ defmodule DtCore.EventLogger do
     {:noreply, state}
   end
 
-  def handle_info({:bridge_ev, _, {op, ev = %DetectorExitEv{}}}, state) do
+  defp on_info({:bridge_ev, _, {op, ev = %DetectorExitEv{}}}, state) do
     operation = Atom.to_string(op)
     %{type: "detector_exit", operation: operation, details: ev}
     |> save_eventlog()
@@ -62,7 +68,7 @@ defmodule DtCore.EventLogger do
     {:noreply, state}
   end
 
-  def handle_info({:bridge_ev, _, {op, ev = %DetectorEntryEv{}}}, state) do
+  defp on_info({:bridge_ev, _, {op, ev = %DetectorEntryEv{}}}, state) do
     operation = Atom.to_string(op)
     %{type: "detector_entry", operation: operation, details: ev}
     |> save_eventlog()
@@ -70,33 +76,33 @@ defmodule DtCore.EventLogger do
     {:noreply, state}
   end
 
-  def handle_info({:bridge_ev, _, {op, ev = %PartitionEv{}}}, state) do
+  defp on_info({:bridge_ev, _, {op, ev = %PartitionEv{}}}, state) do
     operation = Atom.to_string(op)
     %{type: "alarm", operation: operation, details: ev} |> save_eventlog()
     {:noreply, state}
   end
 
-  def filter({_, {_, %ArmEv{}}}) do
+  defp filter({_, {_, %ArmEv{}}}) do
     true
   end
 
-  def filter({_, {_, %DetectorEv{}}}) do
+  defp filter({_, {_, %DetectorEv{}}}) do
     true
   end
 
-  def filter({_, {_, %DetectorExitEv{}}}) do
+  defp filter({_, {_, %DetectorExitEv{}}}) do
     true
   end
 
-  def filter({_, {_, %DetectorEntryEv{}}}) do
+  defp filter({_, {_, %DetectorEntryEv{}}}) do
     true
   end
 
-  def filter({_, {_, %PartitionEv{}}}) do
+  defp filter({_, {_, %PartitionEv{}}}) do
     true
   end
 
-  def filter({_, {_, %ExitTimerEv{}}}) do
+  defp filter({_, {_, %ExitTimerEv{}}}) do
     true
   end
 
