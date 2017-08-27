68a77fdcbbf0bb3a4e02ee987ac4dd274190a4d3
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2017-08-27 16:56:55 +0200

    set ecto_repos config

--- config/config.exs
+++ config/config.exs
@@ -57,6 +57,9 @@ config :guardian, Guardian,
 # Import notification specific configuration
 import_config "notifications.exs"
 
+# Ecto repos
+config :detectino, ecto_repos: [DtCtx.Repo]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
