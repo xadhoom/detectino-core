71136aa04f8a4ffe1a45f12a998e4d7a46706f21
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2018-10-21 19:10:29 +0200

    chore: enforce line len

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,6 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 98,
+  rename_deprecated_at: "1.7.0"
 ]
