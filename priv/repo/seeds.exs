185a87584c7c7058469ffc05fce9c904772af05f
Matteo Brancaleoni <mbrancaleoni@gmail.com>
2019-08-24 16:05:48 +0200

    don't add data in migrations, use seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias DtCtx.Accounts.User
+
+admin_user = %User{
+  name: "admin",
+  username: "admin@local",
+  password: Bcrypt.hash_pwd_salt("password"),
+  role: "admin",
+  pin: "666666"
+}
+
+DtCtx.Repo.insert!(admin_user)
